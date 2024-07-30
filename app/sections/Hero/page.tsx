22f1d6632f930803e9cca73df4c535a4d10d2f02
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-07-30 13:34:38 +0000

    Add lazy loading for images in Hero section

--- app/sections/Hero/page.tsx
+++ app/sections/Hero/page.tsx
@@ -41,31 +41,37 @@ const data = [
     category: "Artificial Intelligence",
     title: "You can do more with AI.",
     src: "https://res.cloudinary.com/dvfmse8he/image/upload/v1722345411/smartmockups_lz8fz68m_kbl2w2.jpg",
+    content: null,
   },
   {
     category: "Productivity",
     title: "Enhance your productivity.",
     src: "https://res.cloudinary.com/dvfmse8he/image/upload/v1722345531/smartmockups_lz8fzuiz_nhfzfy.jpg",
+    content: null,
   },
   {
     category: "Product",
     title: "Launching the new Apple Vision Pro.",
     src: "https://res.cloudinary.com/dvfmse8he/image/upload/v1722345531/smartmockups_lz8g0z3m_d8d7yc.jpg",
+    content: null,
   },
 
   {
     category: "Product",
     title: "Maps for your iPhone 15 Pro Max.",
     src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: null,
   },
   {
     category: "iOS",
     title: "Photography just got better.",
     src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: null,
   },
   {
     category: "Hiring",
     title: "Hiring for a Staff Software Engineer",
     src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: null,
   },
 ];
